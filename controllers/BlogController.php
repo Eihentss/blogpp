2b90efb2760642d87d50897a23fc7c2298769438
Eihentss <125543962+Eihentss@users.noreply.github.com>
2025-02-26 11:27:07 +0200

    Add files via upload

--- controllers/BlogController.php
+++ controllers/BlogController.php
@@ -1,6 +1,7 @@
 <?php
 require "models/Blog.php";
 
+
 class BlogController
 {
   public function index()
@@ -8,4 +9,45 @@ class BlogController
     $posts = Blog::all();
     require "views/blog/index.view.php";
   }
+  public function show()
+  {
+    $id = $_GET['id'];
+    $post = Blog::find($id);
+    require "views/blog/show.view.php";
+  }
+  public function create()
+  {
+    require "views/blog/create.view.php";
+  }
+  public function store()
+  {
+    $content = $_POST['content'];
+    $post = new Blog();
+    $post->content = $content;
+    $post->save();
+    header("Location: /blog");
+  }
+  public function edit()
+  {
+    $id = $_GET['id'];
+    $post = Blog::find($id);
+    require "views/blog/edit.view.php";
+  }
+  public function update()
+  {
+    $id = $_GET['id'];
+    $content = $_POST['content'];
+    $post = Blog::find($id);
+    $post->content = $content;
+    $post->save();
+    header("Location: /blog");
+  }
+  public function destroy()
+  {
+    $id = $_GET['id'];
+    $post = Blog::find($id);
+    $post->delete();
+    header("Location: /blog");
+  }
 }
+
